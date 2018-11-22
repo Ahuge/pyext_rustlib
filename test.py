3845b64969144370712fa08b16385ccd664218d1
Alex Hughes <ahughes@d2.com>
2018-11-22 20:21:42 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+sys.path.append(os.path.abspath("./target/release"))
+
+from librusttest import rustfib
+
+def pyfib(terms):
+    if terms <= 1:
+        return terms
+    return (pyfib(terms-1) + pyfib(terms-2))
+    
+def timer(cbl, args):
+    s = time.time()
+    try:
+        result = cbl(*args)
+    except:
+        result = None
+    print("Time calling %s took %s" % (cbl.__name__, (time.time() - s)))
+    return result
+
+
+for term in [10, 20, 30, 35, 40]:
+    print("Testing %d..." % term)
+    print("  PyFib:")
+    r = timer(pyfib, [term])
+    print("  RustFib:")
+    r2 = timer(rustfib, [term])
+    if r != r2:
+        print("---ERROR---")
+        print("  PyFib: %d   !=   RustFib: %d" % (r, r2))
